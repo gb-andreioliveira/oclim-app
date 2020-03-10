d4f4740598187ce9db8ca9f4fc438e5522207430
Andrei Oliveira <andrei.oliveira@guiabolso.com.br>
2020-03-10 10:21:07 -0300

    Added tests

--- web.py
+++ web.py
@@ -1,9 +1,18 @@
+#!/usr/bin/env python
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route('/hello')
-def helloIndex():
-    return 'Hello World from Python Flask!'
+@app.route('/')
+def hello_main():
+    return 'Welcome to the initial page!\n'
 
-app.run(host='0.0.0.0', port=8080)
+@app.route('/hello/')
+def hello_world():
+    return 'Hello World!\n'
+
+@app.route('/hello/<username>') # dynamic route
+def hello_user(username):
+    return 'Hey there %s!\n' % username
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)     # open for everyone
